df1f616517b9e04efc5f83884f28d3955b1f85b2
vapppu <veera.hiltunen@gmail.com>
2023-06-23 02:34:13 +0300

    First version of exercise done, but array / tuple props are not ideal yet

--- Luku 1/kurssitiedot/src/App.js
+++ Luku 1/kurssitiedot/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.course}</h1>
     </div>
-  );
+  )
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.parts[0]} />
+      <Part part = {props.parts[1]} />
+      <Part part = {props.parts[2]} />
+    </div>
+  )
+}
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part[0]} {props.part[1]}</p>
+    </div>
+  )
 }
 
-export default App;
+
+const Total = (props) => {
+  let sum = 0
+  for (let num of props.amounts) {
+    sum += num;
+  }
+  return (
+    <div>
+      <p>{sum}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+
+      <Content parts={[[part1, exercises1], [part2, exercises2], [part3, exercises3]]}/>
+
+      <Total amounts = {[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App
