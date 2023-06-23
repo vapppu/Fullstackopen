cc588db9700852ee5f3cd2695198f743bf5b2cc7
vapppu <veera.hiltunen@gmail.com>
2023-06-23 12:35:05 +0300

    const values changed into object array

--- Luku 1/kurssitiedot/src/App.js
+++ Luku 1/kurssitiedot/src/App.js
@@ -11,9 +11,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part = {props.part1} />
-      <Part part = {props.part2} />
-      <Part part = {props.part3} />
+      <Part part = {props.parts[0]} />
+      <Part part = {props.parts[1]} />
+      <Part part = {props.parts[2]} />
     </div>
   )
 }
@@ -43,24 +43,26 @@ const Total = (props) => {
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3}/>
-      <Total amounts = {[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content parts = {parts}/>
+      <Total amounts = {[parts[0].exercises, parts[1]. exercises, parts[2].exercises]} />
     </div>
   )
 }
