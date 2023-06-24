22723d306ad22f857f205ad9dafff0b8e031f08e
vapppu <veera.hiltunen@gmail.com>
2023-06-24 14:39:40 +0300

    Practicing callback functions

--- Harjoitukset/part1/src/App.js
+++ Harjoitukset/part1/src/App.js
@@ -1,48 +1,20 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>the app is used by pressing the buttons</div>
-    )
-  }
-  return (
-    <div>button press history: {props.allClicks.join(' ')}</div>
-  )
+
+const App = (props) => {
+
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
 }
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-  )
-
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const  handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-
-
-
-    setRight(right + 1)
-  }
-
   return (
     <div>
-      <div>
-        {left}
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right'/>
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      {value}
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
   )
 }
