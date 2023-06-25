0a5977c97d9ea4443286e03a37dde46edbb8070a
vapppu <veera.hiltunen@gmail.com>
2023-06-25 16:26:57 +0300

    Make sure all updates are pushed to remote

--- Harjoitukset/part1/src/App.js
+++ Harjoitukset/part1/src/App.js
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  <button onClick= {props.handleClick}>{props.text}</button>
+}
+
+const Display = (props) => <div>{props.value}</div>
 
 const App = (props) => {
 
   const [value, setValue] = useState(10)
-
   const setToValue = (newValue) => {
     console.log('value now', newValue)
     setValue(newValue)
 }
   return (
     <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>thousand</button>
+      <Display value={value}/>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
       <button onClick={() => setToValue(0)}>reset</button>
       <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
