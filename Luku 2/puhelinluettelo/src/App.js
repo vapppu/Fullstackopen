5e0a0350c6a99ad07e8032819f1947294b5017c2
vapppu <veera.hiltunen@gmail.com>
2023-06-26 13:48:52 +0300

    Alert if user tries to add name that already exists in phonebook

--- Luku 2/puhelinluettelo/src/App.js
+++ Luku 2/puhelinluettelo/src/App.js
@@ -12,13 +12,16 @@ const App = () => {
       name: newName,
     };
 
+    if (persons.map((person) => (person.name)).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     setPersons(persons.concat(newPerson));
     setNewName("");
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
-    console.log(event.target.value);
   };
 
   return (
