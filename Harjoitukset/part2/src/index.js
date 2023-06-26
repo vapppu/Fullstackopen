e73cb9c902e9a3ce4dd2026c81fa76eb290a0737
vapppu <veera.hiltunen@gmail.com>
2023-06-26 20:29:46 +0300

    Get data from JSON server with axios

--- Harjoitukset/part2/src/index.js
+++ Harjoitukset/part2/src/index.js
@@ -3,27 +3,17 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+import axios from 'axios'
 
-const result = notes.map(note => note.content)
-console.log(result)
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <App notes={notes} />
+    )
+})
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+
+
