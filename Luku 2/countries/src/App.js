4b60ee3e4d84a3eeaa8a531b597db92d62142934
vapppu <veera.hiltunen@gmail.com>
2023-07-06 19:21:04 +0300

    Add Weather section tht fetches capital city's weather data via API

--- Luku 2/countries/src/App.js
+++ Luku 2/countries/src/App.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import SearchBox from "./components/SearchBox";
 import Countries from "./components/Countries";
 import Country from "./components/Country";
+import Weather from "./components/Weather";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [countries, setCountries] = useState([]);
   const [countriesToShow, setCountriesToShow] = useState([]);
   const [countryToShow, setCountryToShow] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     axios
@@ -21,9 +23,7 @@ const App = () => {
   }, []);
 
   const handleInputChange = (event) => {
-
     const userInput = event.target.value;
-
     setSearchTerm(userInput);
 
     console.log(`Filtering results with ${userInput}`);
@@ -37,18 +37,41 @@ const App = () => {
     if (filteredCountries.length === 1) {
       console.log(`One country found, rendering data`);
       setCountryToShow(filteredCountries[0]);
+      getWeather(filteredCountries[0]);
     } else {
       setCountryToShow(null);
+      setWeather(null);
     }
 
     setCountriesToShow(filteredCountries);
-
   };
 
-    const showCountry = (country) => {
-      console.log(`Showing country ${country}`)
-        setCountryToShow(country)
-      }
+  const getWeather = (country) => {
+
+    const api_key = process.env.REACT_APP_API_KEY
+
+    const capital = country.capital[0];
+    console.log(capital);
+
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${capital}&aqi=no`
+      )
+      .then((response) => {
+        console.log(`response: ${response}`);
+        console.log(`Data found: ${response.data}`);
+        setWeather(response.data);
+        console.log(response.data.location);
+      })
+      .catch((error) => {
+        console.log("Error happened!");
+      });
+  };
+
+  const showCountry = (country) => {
+    console.log(`Showing country ${country}`);
+    setCountryToShow(country);
+  };
 
   return (
     <div>
@@ -56,9 +79,16 @@ const App = () => {
 
       <SearchBox inputChanged={handleInputChange} />
 
-      <Countries countriesToShow={countriesToShow} countryToShow = {countryToShow} term={searchTerm} showCountry = {showCountry}/>
+      <Countries
+        countriesToShow={countriesToShow}
+        countryToShow={countryToShow}
+        term={searchTerm}
+        showCountry={showCountry}
+      />
 
-      <Country country = {countryToShow} />
+      <Country country={countryToShow} weather={weather} />
+
+      <Weather weather={weather} />
 
     </div>
   );
--- Luku 2/countries/src/components/Countries.js
+++ Luku 2/countries/src/components/Countries.js
@@ -1,30 +1,34 @@
 const Countries = ({ countriesToShow, term, showCountry, countryToShow }) => {
-    const number = countriesToShow.length;
+  const number = countriesToShow.length;
 
-    if (number === 1)
-    {
-        return
-    };
-  
-    if (number <= 10) {
-      return (
-        <div>
-          {countriesToShow.sort().map((result) => (
-            <div key={result.name.common}>
-                {result.name.common}
-                <button onClick={() => {
-                    console.log(result)
-                    showCountry(result)}}>Show</button>
-            </div>
-          ))}
-        </div>
-      );
-    }
-    if (term.length > 0) {
-      return <div>Too many matches, specify another filter</div>;
-    }
-  
+  if (number === 1) {
     return;
-  };
+  }
 
-  export default Countries;
+  if (number <= 10) {
+    return (
+      <div>
+        {countriesToShow.sort().map((result) => (
+          <div key={result.name.common}>
+            {result.name.common}
+            <button
+              onClick={() => {
+                console.log(result);
+                showCountry(result);
+              }}
+            >
+              Show
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  if (term.length > 0) {
+    return <div>Too many matches, specify another filter</div>;
+  }
+
+  return;
+};
+
+export default Countries;
